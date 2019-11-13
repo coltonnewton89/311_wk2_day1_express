317e3c827e6b6866affb7654bdb6ce4ca535c3c7
coltonnewton89 <52718393+coltonnewton89@users.noreply.github.com>
2019-11-12 21:00:10 -0600

    commit 2

--- index.js
+++ index.js
@@ -5,10 +5,31 @@ const port = process.env.PORT || 4000
 
 const { users } = require('./state')
 
-/* BEGIN - create routes here */
+app.get('/users', (req, res) => {
+  res.json(users);
+})
 
+app.get('/users/1', (req, res) => {
+  res.json(users[0]);
+})
+
+app.post('/users', function (req, res) {
+  let newUser = {
+    "_id": 6,
+    "name": "Charlie",
+    "occupation": "Bartender"
+  }
+  users.push(newUser);
+  res.json(users);
+
+})
+
+app.put('users/1', (req, res) => {
+  users[0].occupation = 'kung fu fighter';
+  res.json(users)
+})
 
 /* END - create routes here */
 
-app.listen(port, () => 
+app.listen(port, () =>
   console.log(`Example app listening on port ${port}!`))
