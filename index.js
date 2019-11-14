157b5d7e19b855f238ebe58f4edf18d08e042610
coltonnewton89 <52718393+coltonnewton89@users.noreply.github.com>
2019-11-14 16:43:22 -0600

    ant hack

--- index.js
+++ index.js
@@ -1,40 +1,62 @@
-
 const express = require('express')
+var bodyParser = require('body-parser')
 const app = express()
 const port = process.env.PORT || 4000
 
+
 const { users } = require('./state')
 
+let counter = users.length + 1;
+
+/* BEGIN - create routes here */
 app.get('/users', (req, res) => {
-  res.json(users);
+  res.json(users)
 })
+
 
 app.get('/users/1', (req, res) => {
-  res.json(users[0]);
+  res.json(users[0])
 })
 
-app.post('/users', function (req, res) {
+app.post('/users', (req, res) => {
   let newUser = {
-    "_id": 6,
-    "name": "Charlie",
-    "occupation": "Bartender"
+    _id: 6,
+    name: "Charlie",
+    occupation: "Bartender",
   }
   users.push(newUser);
   res.json(users);
 
 })
 
-app.put('users/1', (req, res) => {
-  users[0].occupation = 'kung fu fighter';
-  res.json(users)
+app.put("/users/1", (req, res) => {
+  users[0].occupation = "kung fu fighter";
+  res.json(users[0]);
+});
+
+app.delete("/users/1", (req, res) => {
+  users[0]._id = null;
+  res.send("DELETED");
+});
+
+app.post("/users", (req, res) => {
+  let hardCode = {
+    _id: counter,
+    name: "Teddy",
+    occupation: "KBG Gangster",
+  };
+  users.push(hardCode);
+  res.json(hardCode);
+  console.log(req.body)
+
+
 })
 
-app.delete('users/1', function (req, res) {
-  res.send('Deleted')
-})
+
+
+
 
 /* END - create routes here */
-
 
 app.listen(port, () =>
   console.log(`Example app listening on port ${port}!`))
